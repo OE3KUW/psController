37977c8eb6b3d7427628d94f9942d32519352203
OE3KUW <wolfgang.kuran@htlstp.ac.at>
2023-01-26 19:59:36 +0100

    TimerInterrupt implemented

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,41 @@
 ******************************************************************/
 #include <Arduino.h>
 #include <PS4Controller.h>
-//#include <WiFi.h>
+
+#define TRUE                             true
+#define FALSE                            false
+#define WAIT_ONE_SEC                     10000
+
+volatile int oneSecFlag;
+hw_timer_t *timer = NULL;
+void IRAM_ATTR myTimer(void);
 
 void setup() 
 {
     Serial.begin(115200);
     Serial.println("start!");
-//    PS4.begin("90:cd:b6:e9:bb:50");
+
+    timer = timerBegin(0, 80, true);
+    timerAttachInterrupt(timer, &myTimer, true);
+    timerAlarmWrite(timer, 100, true);  // 0.1 msec
+    timerAlarmEnable(timer);
+    
+    oneSecFlag = FALSE; 
     PS4.begin("10:20:30:40:50:62");
     Serial.println("ready!");
 }
 void loop() 
 {
     static int t1 = 0;
-    if ( millis() - t1 > 1000)
+    if (oneSecFlag)
     {
-        t1 = millis();   
+        oneSecFlag = FALSE;
+
         if(PS4.isConnected())
         {
             Serial.println("connected!");
             if(PS4.Triangle()) Serial.println("Triangle!");
+            if(PS4.Up()) Serial.println("Up!");
         }
         else
         {
@@ -31,3 +46,20 @@ void loop()
         }
     }    
 }    
+
+//** Timer Interrupt:   ******************************************
+
+void IRAM_ATTR myTimer(void)   // periodic timer interrupt, expires each 0.1 msec
+{
+    static int32_t tick = 0;
+    
+    tick++;
+
+    if (tick >= WAIT_ONE_SEC) 
+    {
+        oneSecFlag = TRUE;
+        tick = 0; 
+    }
+}
+
+
