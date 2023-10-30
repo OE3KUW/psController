88758e2d36d9f5c2effd40fb6bd878a086ce7a0e
OE3KUW <wolfgang.kuran@htlstp.ac.at>
2023-10-30 21:09:44 +0100

    2.3 WebSocket WebServer - 270 ff - just not fine

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,49 @@
                         LoLin32 psController 
                                                     қuran nov 2023
 ******************************************************************/
+// im file Fastled.h 
+// #ifdef SmartMatrix_h   warum auch immer - abgeschalten! 
+
 #include <Arduino.h>
 #include <PS4Controller.h>
 
 #define FASTLED_ALL_PINS_HARDWARE_SPI
 #include <FastLED.h>
 
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "SPIFFS.h"
+
+
+// -------  defines -----------------------------------------------
+
 #define TRUE                            true
 #define FALSE                           false
 #define WAIT_ONE_SEC                    10000
+#define WAIT_250_MSEC                   2500
 #define WAIT_ONE_10MSEC                 100
 
 #define H                               HIGH
 #define L                               LOW
 
-#define ON_BOARD_LED                    5
+//  --- #define ON_BOARD_LED                    5
 
 #define NUM_LEDS                        4
 #define DATA_PIN                        23
 #define CLOCK_PIN                       18
-
 
 #define WHEEL_L                         2
 #define WHEEL_R                         A4
 #define WHEEL_L_DIRECTION               15 
 #define WHEEL_R_DIRECTION               A5
 
+#define BATTERY_LEVEL                   A3      // GPIO 39
 
+#define REFV                            685.0     // factor
+
+
+// -------  global Variables --------------------------------------
 
 volatile int oneSecFlag;
 volatile int tenMSecFlag;
@@ -44,6 +60,120 @@ volatile int pulse;
 volatile int connected;
 volatile int LDir;
 volatile int RDir;
+
+volatile int pointer;
+
+const char *ssid = "A1-A82861";
+const char *password = "7PMGDV96J8";
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+
+//IPAddress lclIP(192,168,2,219);
+//IPAddress gateway(192,168,2,1);
+//IPAddress subnet(255,255,255,0);
+
+
+const int led5 = 5;
+bool ledState = 0;
+
+// -------  functions ---------------------------------------------
+
+void initSPIFFS()
+{
+    if (!SPIFFS.begin(true))
+    {
+        Serial.println("An error has occurred while mounting SPIFFS");
+    }
+    else
+    {
+        Serial.println("SPIFFS mounted successfully!");
+    }
+}
+
+void initWiFi()
+{
+    //WiFi.softAPConfig(lclIP, gateway, subnet);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.println("Connection to WiFi . . .");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(" .");
+        delay(1000);
+    }
+    Serial.println(WiFi.localIP());
+}
+
+String processor(const String& var)
+{
+    if (var == "STATE") 
+    {
+        if (digitalRead(led5))
+        {
+            ledState = 1; return "ON";
+        }
+        else
+        {
+            ledState = 0; return "OFF";
+        }
+    }
+    return String();
+}
+
+void notifyClients(String state)
+{
+    ws.textAll(state);
+}
+
+void handleWebSocketMessage(void *arg, uint8_t * data, size_t len)
+{
+    AwsFrameInfo * info = (AwsFrameInfo*)arg;
+    if (info->final && info->index == 0 && info->len && info->opcode == WS_TEXT)
+    {
+        data[len] = 0;
+        if (strcmp((char*)data, "bON") == 0)
+        {
+            ledState = 1;
+            notifyClients("ON");
+        }
+        if (strcmp((char*)data, "bOFF") == 0)
+        {
+            ledState = 0;
+            notifyClients("OFF");
+        }
+    }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient * client, AwsEventType type, 
+             void * arg, uint8_t * data, size_t len)
+{
+    switch(type)
+    {
+        case WS_EVT_CONNECT: 
+             Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+        break;
+
+        case WS_EVT_DISCONNECT:
+             Serial.printf("WebSocket client #%u disconnected\n", client->id());
+        break;
+
+        case WS_EVT_DATA:
+             handleWebSocketMessage(arg, data, len);
+        break;
+
+        case WS_EVT_PONG:
+        case WS_EVT_ERROR:
+        break;
+    }
+}
+
+void initWebSocket()
+{
+    ws.onEvent(onEvent);
+    server.addHandler(&ws);
+}
 
 
 void setup() 
@@ -61,14 +191,17 @@ void setup()
     PS4.begin("10:20:30:40:50:62");
     Serial.println("ready!");
 
-    pinMode(ON_BOARD_LED, OUTPUT);
+    // -- pinMode(ON_BOARD_LED, OUTPUT);
+    pinMode(led5, OUTPUT);
 
     pinMode(WHEEL_L, OUTPUT);
     pinMode(WHEEL_R, OUTPUT);
     pinMode(WHEEL_L_DIRECTION, OUTPUT);
     pinMode(WHEEL_R_DIRECTION, OUTPUT);
+    
+    pinMode(BATTERY_LEVEL, INPUT);
 
-    digitalWrite(ON_BOARD_LED, L); // on ! ... blue 
+    // -- digitalWrite(ON_BOARD_LED, L); // on ! ... blue 
 
     digitalWrite(WHEEL_L_DIRECTION, L);
     digitalWrite(WHEEL_R_DIRECTION, H);
@@ -83,6 +216,7 @@ void setup()
     connected = 0;
     LDir = 0;
     RDir = 1;
+    pointer = 0;
 
     vL = vR = 0;
 
@@ -98,15 +232,33 @@ void setup()
     FastLED.show();
 
 
+    initSPIFFS();
+    initWiFi();
+    initWebSocket();
 
 
+    server.on("/", HTTP_GET,
+    [](AsyncWebServerRequest * request)
+    {
+        request->send(SPIFFS, "/index.html", "text/html", false, processor);
+    });
 
+    server.serveStatic("/", SPIFFS, "/");
+
+    // Start server:
+
+    server.begin();
 }
+
 void loop() 
 {
-  uint8_t leftstickXDATA0 = 0;
-  uint8_t leftstickXDATA1 = 0;
-  static int t1 = 0;
+    uint8_t leftstickXDATA0 = 0;
+    uint8_t leftstickXDATA1 = 0;
+    float x;
+    static int t1 = 0;
+
+    ws.cleanupClients();
+    digitalWrite(led5, ledState);
 
     if (oneSecFlag)
     {
@@ -114,13 +266,18 @@ void loop()
 
         pulse = pulse ? 0 : 1;
 
-        digitalWrite(ON_BOARD_LED, pulse);
+        //digitalWrite(ON_BOARD_LED, pulse);
+        
+        x = analogRead(BATTERY_LEVEL) / REFV;
+
+        // Serial.print(x); Serial.println(" V");
 
 
         if(PS4.isConnected())
         {
             connected = 1;
-            digitalWrite(ON_BOARD_LED, pulse);  // off
+
+            // digitalWrite(ON_BOARD_LED, pulse);  // off
 
             leds[0] = CRGB{0, 0, 255}; // R B G
             leds[1] = CRGB{0, 255, 0};
@@ -129,10 +286,6 @@ void loop()
 
             FastLED.show();
 
-
-
-
-            //Serial.println("connected!");
             if(PS4.Triangle()) Serial.println("Triangle!"); 
             if(PS4.Up()) Serial.println("Up!");
 
@@ -142,19 +295,48 @@ void loop()
             //Serial.print(PS4.GyrX());
             //Serial.println(PS4.GyrY());
             
-            //vL = PS4.L2Value();
-            //vR = PS4.R2Value();
         }
         else
         {
-         
-            
+            //Serial.print("*");
 
-             //Serial.print("*");
+            pointer++; if (pointer >= 4) pointer = 0;
+
+            switch(pointer)
+            {
+                case 0:
+                    leds[0] = CRGB{255, 255, 255}; // R B G
+                    leds[1] = CRGB{255, 255, 255};
+                    leds[2] = CRGB{0, 0, 0};  // Yellow: FF00FF
+                    leds[3] = CRGB{0, 0, 0};
+                break;
+
+                case 1:
+                    leds[0] = CRGB{0, 0, 0}; // R B G
+                    leds[1] = CRGB{255, 255, 255};
+                    leds[2] = CRGB{255, 255, 255};  // Yellow: FF00FF
+                    leds[3] = CRGB{0, 0, 0};
+                break;
+
+                case 2:
+                    leds[0] = CRGB{0, 0, 0}; // R B G
+                    leds[1] = CRGB{0, 0, 0};
+                    leds[2] = CRGB{255, 255, 255};  // Yellow: FF00FF
+                    leds[3] = CRGB{255, 255, 255};
+                break;
+
+                case 3:
+                    leds[0] = CRGB{255, 255, 255}; // R B G
+                    leds[1] = CRGB{0, 0, 0};
+                    leds[2] = CRGB{0, 0, 0};  // Yellow: FF00FF
+                    leds[3] = CRGB{255, 255, 255};
+                break;
+
+            }
+
+            FastLED.show();
+
         }
-
-      
-
     }    
 
     if (tenMSecFlag)
@@ -172,8 +354,6 @@ void loop()
     {
         vL = PS4.L2Value();
         vR = PS4.R2Value();
-
-
     }
 
 }    
@@ -190,7 +370,7 @@ void IRAM_ATTR myTimer(void)   // periodic timer interrupt, expires each 0.1 mse
     mtick++;
     ramp++;
 
-    if (tick >= WAIT_ONE_SEC) 
+    if (tick >= WAIT_250_MSEC) 
     {
         oneSecFlag = TRUE;
         tick = 0; 
@@ -208,3 +388,11 @@ void IRAM_ATTR myTimer(void)   // periodic timer interrupt, expires each 0.1 mse
 }
 
 
+
+/* ------ für ein anderes Project ... Siehe Build Web Servers Seite 270 ff... 
+
+board_build.partitions = huge_app.csv 
+
+
+
+*/
